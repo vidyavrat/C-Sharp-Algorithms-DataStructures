cccc2e4588e96b170528b69363fd75b6d3089c88
vidyavrat <vidya_mct@yahoo.com>
2017-04-19 09:59:54 -0700

    Changed folder name

--- Sorting/BubbleSort.cs
+++ Sorting/BubbleSort.cs
@@ -15,7 +15,7 @@ namespace Sorting
             }
             for (int i = a.Length - 1; i > 0; i--)
             {
-                for (int j = 0; j <= i - 1; j++)
+                for (int j = 0; j <= i-1; j++)
                 {
                     if (a[j] > a[j + 1])
                     {
