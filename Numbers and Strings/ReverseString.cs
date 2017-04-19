3890588f2bc69ba7b4cba8e4bbfbceeb95dc96df
vidyavrat <vidya_mct@yahoo.com>
2017-04-19 10:06:34 -0700

    Added New Project Strings and Numbers

---
+++ Numbers and Strings/ReverseString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Strings
+{
+    public class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            var str = "aBcD";
+            var newStr = string.Empty;
+
+            // Reverese string
+            for (var i = str.Length - 1; i >= 0; i--)
+            {
+                newStr += str[i];
+            }
+            Console.WriteLine(newStr);
+            
+            Console.ReadLine();
+        }
+    }
+}
