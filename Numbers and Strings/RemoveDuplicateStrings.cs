758f4de3f606c7bdf0eccfde75bb107b5e743466
Vidya Vrat Agarwal <vidya_mct@yahoo.com>
2017-05-03 09:58:23 -0700

    UniqueCharacters In String and Remove Duplicates from a String.

---
+++ Numbers and Strings/RemoveDuplicateStrings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strings
+{
+    public class RemoveDuplicateStrings
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[] {
+                "vidya",
+                "Vrat",
+                "Agarwal",
+                "vidya",
+                "vrat",
+                "arshika",
+                "pearly",
+                "seattle"
+            };
+
+            Console.WriteLine($"Total Count of Strings in Array {names.Length}");
+
+            RemoveDuplicateString(names);
+
+            Console.ReadLine();
+        }
+
+        public static void RemoveDuplicateString(string[] strAray)
+        {
+            HashSet<string> hset = new HashSet<string>(strAray);
+
+            foreach(var s in hset)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine($"Total Count after removing Duplicates {hset.Count.ToString()}");            
+        }
+    }
+}
+
---
+++ Numbers and Strings/UniqueCharacterString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Strings
+{
+    public class UniqueCharacterString
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string : ");
+            var s = Console.ReadLine();
+
+            Console.WriteLine(IsUniqueCharacherString(s));
+
+            Console.ReadLine();
+        }
+
+        public static bool IsUniqueCharacherString(string str)
+        {
+            Dictionary<char, int> kv = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (kv.ContainsKey(c))
+                {
+                    return false;
+                }
+                else
+                {
+                    kv.Add(c, 1);
+                }
+            }
+            return true;
+        }
+    }
+}
