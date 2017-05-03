99293d98313f47bda2954c10fcb9a4c343c40ce7
Vidya Vrat Agarwal <vidya_mct@yahoo.com>
2017-05-02 22:51:44 -0700

    Fibonacci and ReverseWordsUsingStack

---
+++ Numbers and Strings/FibonacciSeries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndNumbers
+{
+    class FibonacciSeries
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter lenth of Series : ");
+            var f = Int32.Parse(Console.ReadLine());
+
+            for(int i=0; i <= f;i++)
+            {
+                Console.Write(FibonacciNumber(i));
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int FibonacciNumber(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return FibonacciNumber(n - 1) + FibonacciNumber(n - 2);
+        }
+
+    }
+}
---
+++ Numbers and Strings/ReverseWordsInStringUsingStack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringsAndNumbers
+{
+    public class ReverseWordsInStringUsingStack
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            string x = "This is test to reverse string";
+            Console.WriteLine($"Original string: {x}");
+            //Trim whitespace from beginning and end of string
+            x = x.Trim();
+
+            //Necessary because foreach will execute once with empty string returning 1
+            //if (x == "")
+            //    return 0;
+
+            //Ensure there is only one space between each word in the passed string
+            while (x.Contains("  "))
+                x = x.Replace("  ", " ");
+
+            var wordStack = new Stack<string>();
+            
+            //push words into the stack
+            foreach (string y in x.Split(' '))
+            {
+                wordStack.Push(y);
+                wordStack.Push(" ");               
+            }
+          
+            Console.WriteLine($"Total Words in Stack : {wordStack.Count}");
+
+            var sb = new StringBuilder();
+            while(wordStack.Count >0)
+            {
+                sb.Append(wordStack.Pop());
+            }
+            Console.WriteLine(sb.ToString().Trim());
+
+            Console.ReadLine();
+        }
+    }
+}
