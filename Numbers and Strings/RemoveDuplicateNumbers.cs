20a4a6fc776445ccbe2c6760441b88b0c5c8e55a
Vidya Vrat Agarwal <vidya_mct@yahoo.com>
2017-05-03 10:49:19 -0700

    RemoveDuplicateNumbers

---
+++ Numbers and Strings/RemoveDuplicateNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Numbers
+{
+    public class RemoveDuplicateNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] {1,2,2,3,4,5,6,6,7,8,9,9};
+
+            Console.WriteLine($"Total Count of Strings in Array {num.Length}");
+
+            RemoveDuplicateNumber(num);
+
+            Console.ReadLine();
+        }
+
+        public static void RemoveDuplicateNumber(int[] numAray)
+        {
+            HashSet<int> hset = new HashSet<int>(numAray);
+
+            foreach (var n in hset)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine($"Total Count after removing Duplicates {hset.Count.ToString()}");
+        }
+    }
+}
+
