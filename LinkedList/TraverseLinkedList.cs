a42ae37bc7cb2a5897a95d8491fc2cb0f0e3d748
Vidya Vrat Agarwal <vidya_mct@yahoo.com>
2017-05-13 23:49:42 -0700

    TraverseLinkedList

---
+++ LinkedList/TraverseLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Node
+    {
+        public int data { get; set; }
+        public Node Next { get; set; }
+    }
+
+    class TraverseLinkedList
+    {
+        static void Main(string[] args)
+        {
+            var first = new Node { data = 1 };
+            var middle = new Node { data = 2 };
+            var last = new Node { data = 3 };
+
+            first.Next = middle;
+            middle.Next = last;
+
+            // iterate through linked-list
+            Node node = first;
+            while(node !=null)
+            {
+                Console.WriteLine(node.data);
+                node = node.Next;
+            }
+
+            Console.ReadLine();            
+        }
+    }
+}
