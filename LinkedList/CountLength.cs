6056fd0083f8f9c38719deecf3fcd175cdc14d58
Vidya Vrat Agarwal <vidya_mct@yahoo.com>
2017-05-15 00:14:12 -0700

    Count Length

---
+++ LinkedList/CountLength.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class CountLength
+    {
+        static void Main(string[] args)
+        {
+            Console.Write($"Lenth of linkedList 1->2->3 : {Lenth()}");
+            Console.ReadLine();
+        }
+
+        public static int Lenth()
+        {
+            // create a LinkedList 1-> 2-> 3
+            var first = new Node(1);
+            var middle = new Node(2);
+            var last = new Node(3);
+
+            first.Next = middle;
+            middle.Next = last;
+
+            if (first == null)
+            {
+                return 0;
+            }
+
+            // traverse linkedlist until end 
+            Node temp = first;
+            int count = 1;
+            while (temp.Next != null)
+            {
+                count++;
+                temp = temp.Next;
+            }
+
+           return count;            
+        }
+    }
+}
