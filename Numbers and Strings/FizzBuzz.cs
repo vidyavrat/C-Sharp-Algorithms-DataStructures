f747fa952d49dbb5e20df892a3a5d0283f348c23
Vidya Vrat Agarwal <vidya_mct@yahoo.com>
2017-05-16 13:02:50 -0700

    FizzBuzz

---
+++ Numbers and Strings/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Numbers
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter lenth of Series : ");
+            var length = Int32.Parse(Console.ReadLine());
+
+            PintFizzBuzz(length);
+            
+            Console.ReadLine();
+        }
+
+        public static void PintFizzBuzz(int length)
+        {
+            if (length <= 0)
+            {
+                Console.WriteLine("no data");
+            }
+
+            for (int i=1; i<=length; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+
+                if(fizz)
+                {
+                    Console.WriteLine($"{i} : Fizz");
+                }
+                else if(buzz)
+                {
+                    Console.WriteLine($"{i} : Buzz");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} FizzBuzz");
+                }
+            }
+        }
+    }
+}
